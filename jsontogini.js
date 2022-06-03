57d8cbd2e4fb243c2d0dc5509f97079bcb7d4e95
Decode <82981812+iamGodskid@users.noreply.github.com>
2022-06-03 04:51:52 +0100

    Update jsontogini.js

--- jsontogini.js
+++ jsontogini.js
@@ -36,64 +36,12 @@ $$$.forEach(k=>{
      result += `${s[0]}=${s[1]}\n`;
     arr.push(result)
       arr.reverse()
-      gini(arr[0])
+      gini(arr)
     })
   }
   
   
 })
-
-  }
-  
-  //for fetching from file
-  
-  
-   else if(typeof param == "string"){
-     xml.open("GET", param, true);
- xml.onreadystatechange=function(){
-   if(this.readyState==4){
-var response = this.responseText;
-  console.log(response)
-  $data=response;
-  const $$=JSON.parse($data);
-  console.log($$)
-  
-  
- let result=""
-  //loop and transpile
-const $obj=Object.entries($$);
-const $$$ = Object.keys($$)
-let arr=[];
-$$$.forEach(k=>{
-  if(typeof $$[k] !== "object"){
-    arr.push(`[${k}]\n` );
-    result += `____________________\n[${k}]\n`;
-    arr.push(`${k}=${$$[k]}\n____________________\n`)
-      result += `${k}=${$$[k]}\n`;
-    
-  const $gini=arr.push(result)
-    arr.reverse()
-    gini(arr)
-  }else if(typeof $$[k] == "object"){
-    //if typeof counters
-    const subs=Object.entries($$[k])
-    result += `____________________\n[${k}]\n`;
-    subs.forEach(s=>{
-     result += `${s[0]}=${s[1]}\n`;
-    arr.push(result)
-      arr.reverse()
-      gini(arr[0])
-    })
-  }
-  
-  
-})
-
-
-   }
- }
- xml.send() 
-  
 
   }
   })
