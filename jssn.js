503a3e5b5bab2fee0cd58446f583fe2253b101f8
Decode <82981812+iamGodskid@users.noreply.github.com>
2022-09-12 05:24:45 +0100

    Update jssn.js

--- jssn.js
+++ jssn.js
@@ -3,9 +3,11 @@ function parseJSSNFile(opt) {
 let uri=opt
 let result = {};
 let section = result;
-let invalid=!/^\s*(!._*)?$/
+let invalid=/^\/\*(.*)\*\/$/gim
 var xml;
-let verify=/^<@JSSN@>/gi
+let reg=/^\.|^\/|^http:\/\/|^https:\/\/|^(.*)\.(.*)/gim
+let verify=/^<@GINI@>/gi
+console.log(reg.test(uri))
 if(window.XMLHttpRequest){
   xml=new XMLHttpRequest()
 }else{
@@ -24,13 +26,35 @@ if(verify.test(string)){
 }
 string.split(/\r?\n/).forEach(line => {
 let match;
+let matcharr = [];
 if (match = line.match(/^(\w+)=(.*)$/)) {
-section[match[1]] = match[2];
-} else if (match = line.match(/^\[(.*)\]$/)) {
+  /*
+  process for jssn arrays
+  */
+if(/{(.*)}/.test(match[2])==true){
+  //console.log(match[1])
+ const toarr= match[2].replace(/{|}/gim, "");
+ toarr.split(/\r?\t?\s/).forEach(array_in=>{
+ //  console.log(array_in);
+   matcharr.push(array_in)
+ })
+  section[match[1]]=matcharr;
+}
+//for comment
+else if(/\/\*(.*)\*\//gim.test(match[2])==true){
+  section[match[1]]=""
+}
+//end
+else{
+  section[match[1]]=match[2]
+}
+//end
+}
+else if (match = line.match(/^\[(.*)\]$/)) {
 section = result[match[1]] = {};
 } 
 
-else if (!/^\s*(!._*)?$/.test(line)) {
+else if (!/^\/\*(.*)\*\/$/.test(line)) {
 line.replace(invalid, "");
 }
 });
